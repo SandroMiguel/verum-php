7418204a548024157754b1962f18871f7f4e8817
SandroMiguel <sandromiguel@sandromiguel.com>
2020-09-21 16:26:33 +0100

    test(between_length): add more tests

--- tests/BetweenLengthTest.php
+++ tests/BetweenLengthTest.php
@@ -10,7 +10,7 @@
  * @author    Sandro Miguel Marques <sandromiguel@sandromiguel.com>
  * @copyright 2020 Sandro
  * @since     Verum-PHP 1.0.0
- * @version   1.2.0 (10/06/2020)
+ * @version   1.3.0 (2020/06/10)
  * @link      https://github.com/SandroMiguel/verum-php
  */
 
@@ -78,23 +78,13 @@ class BetweenLengthTest extends TestCase
     }
 
     /**
-     * The String ('text with 23 characters') value should violate the rule with length between 50 and 100 characters.
+     * Null value should pass validation (ignored field).
      *
      * @return void
      */
-    public function testValidateLessThanMin(): void
+    public function testValidateNull(): void
     {
-        $this->assertFalse($this->validate('text with 23 characters', [50, 100]));
-    }
-
-    /**
-     * The String ('text with 23 characters') value should violate the rule with length between 10 and 20 characters.
-     *
-     * @return void
-     */
-    public function testValidateGreatThanMax(): void
-    {
-        $this->assertFalse($this->validate('text with 23 characters', [10, 20]));
+        $this->assertTrue($this->validate(null, [10, 20]));
     }
 
     /**
@@ -105,6 +95,110 @@ class BetweenLengthTest extends TestCase
     public function testValidateEmptyString(): void
     {
         $this->assertTrue($this->validate('', [10, 20]));
+    }
+
+    /**
+     * A Zero String ('0') value should not pass validation.
+     *
+     * @return void
+     */
+    public function testValidateZeroString(): void
+    {
+        $this->assertFalse($this->validate('0', [10, 20]));
+    }
+
+    /**
+     * The Zero Number (0) value should not pass validation.
+     *
+     * @return void
+     */
+    public function testValidateZeroNumber(): void
+    {
+        $this->assertFalse($this->validate(0, [10, 20]));
+    }
+
+    /**
+     * A Boolean (false) value should not pass validation.
+     *
+     * @return void
+     */
+    public function testValidateFalse(): void
+    {
+        $this->assertFalse($this->validate(false, [10, 20]));
+    }
+
+    /**
+     * An Empty Array ([]) value should not pass validation.
+     *
+     * @return void
+     */
+    public function testValidateEmptyArray(): void
+    {
+        $this->assertFalse($this->validate([], [10, 20]));
+    }
+
+    /**
+     * The Minus One (-1) value should not pass validation.
+     *
+     * @return void
+     */
+    public function testValidateMinusOne(): void
+    {
+        $this->assertFalse($this->validate(-1, [10, 20]));
+    }
+
+    /**
+     * The One (1) value should not pass validation.
+     *
+     * @return void
+     */
+    public function testValidateOne(): void
+    {
+        $this->assertFalse($this->validate(1, [10, 20]));
+    }
+
+    /**
+     * The One (12345) value should not pass validation.
+     *
+     * @return void
+     */
+    public function testValidateFiveDigits(): void
+    {
+        $this->assertTrue($this->validate(12345, [5, 10]));
+    }
+
+    /**
+     * A Boolean (true) value should not pass validation.
+     *
+     * @return void
+     */
+    public function testValidateTrue(): void
+    {
+        $this->assertFalse($this->validate(true, [10, 20]));
+    }
+
+    /**
+     * The String ('text with 23 characters') value should violate the rule with length between 50 and 100 characters.
+     *
+     * @return void
+     */
+    public function testValidateLessThanMin(): void
+    {
+        $this->assertFalse(
+            $this->validate('text with 23 characters', [50, 100])
+        );
+    }
+
+    /**
+     * The String ('text with 23 characters') value should violate the rule with length between 10 and 20 characters.
+     *
+     * @return void
+     */
+    public function testValidateGreatThanMax(): void
+    {
+        $this->assertFalse(
+            $this->validate('text with 23 characters', [10, 20])
+        );
     }
 
     /**
